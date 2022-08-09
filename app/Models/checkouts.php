6df1f7219bb85ff3bafd673ca215898dd4816302
WP-HAKIM <hakimluk0154@gmail.com>
2022-08-09 07:23:59 +0700

    Make Seeder For Camp and CampBenefits

---
+++ app/Models/checkouts.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
+class checkouts extends Model
+{
+    use HasFactory,softDeletes;
+
+    protected $fillable = [
+        'card_number',
+        'expired',
+        'cvc',
+        'is_paid'
+    ];
+}
