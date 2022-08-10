9b09d1dbb4579aaf587909cdebc42e407aed171d
WP-HAKIM <hakimluk0154@gmail.com>
2022-08-10 10:43:25 +0700

    Slicing Desain LaraCamp

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('/front-end/pages/index');
 });
+Route::get('/login', function () {
+    return view('/front-end/pages/login');
+});
+Route::get('/checkout', function () {
+    return view('/front-end/pages/checkout');
+});
+Route::get('/success-checkout', function () {
+    return view('/front-end/pages/succescheckout');
+});
+Route::get('/dashboard', function () {
+    return view('/front-end/pages/dashboard');
+});
